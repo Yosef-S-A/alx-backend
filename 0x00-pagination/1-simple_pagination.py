150552c6365987410dc5a8a1b5fb2b85528a261c
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-22 00:27:36 -0500

    Update the docstring

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,7 +27,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> list:
-        """  """
+        """ return tha appropriate page of the dataset """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
         indexes = index_range(page, page_size)
